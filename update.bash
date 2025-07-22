7b51a156864df323c3bf12d76bccd1f89f80f153
Sunev <ccg6612@163.com>
2025-07-22 10:06:26 +0800

    refactor: change forever to pm2

--- update.bash
+++ update.bash
@@ -1,5 +1,5 @@
 #! /bin/bash
-forever stop jinrou
+pm2 stop jinrou
 git reset --hard
 git pull
 # npm ci
@@ -7,4 +7,4 @@ cd front/
 # npm ci
 npm run production-build
 cd ..
-SS_ENV=production SS_PACK=1 forever start -l ../logs/out.log -e ../logs/err.log -a --uid "jinrou" app.js
+SS_ENV=production SS_PACK=1 pm2 start -l ../logs/out.log -e ../logs/err.log -a --name "jinrou" --time app.js
