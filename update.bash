bc4906cfefc2bf3ab0fa64d6d53a2de0679312bf
Sunev <ccg6612@163.com>
2025-07-28 21:11:12 +0800

    fix: 添加定时重启和内存限制到 pm2 启动命令

--- update.bash
+++ update.bash
@@ -7,4 +7,4 @@ cd front/
 # npm ci
 npm run production-build
 cd ..
-SS_ENV=production SS_PACK=1 pm2 start -l ../logs/out.log -e ../logs/err.log -a --name "jinrou" --time app.js
+SS_ENV=production SS_PACK=1 pm2 start -l ../logs/out.log -e ../logs/err.log -a --name "jinrou" --time app.js --cron-restart="0 4 * * *" --max-memory-restart 700M
